e308f0dd43ef70013c6981cdffd1047cbc59b254
Yannick <vanham.yannick@gmail.com>
2020-10-04 23:22:52 +0200

    Set up user details

--- src/main/java/com/upsilonium/securacademy/security/ApplicationSecurityConfig.java
+++ src/main/java/com/upsilonium/securacademy/security/ApplicationSecurityConfig.java
@@ -1,17 +1,22 @@
 package com.upsilonium.securacademy.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author Yannick Van Ham
  * created on Sunday, 04/10/2020
  */
 @EnableWebSecurity
-public class ApplicationSecurityConfig {
+public class ApplicationSecurityConfig{
 
     @Configuration
     @Profile("basicAuth")
@@ -26,5 +31,21 @@ public class ApplicationSecurityConfig {
                     .and()
                     .httpBasic();
         }
+
+        @Override
+        @Bean
+        protected UserDetailsService userDetailsService() {
+            UserDetails richieUser = User.builder()
+                    .username("richie")
+                    .password("password")
+                    .roles("STUDENT")
+                    .build();
+
+            return new InMemoryUserDetailsManager(
+                    richieUser
+            );
+        }
     }
+
+
 }
